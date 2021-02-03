1638cff024168893ab2c4dda3508fcfdaa132f60
luccui000 <luccui2k@gmail.com>
2021-02-03 10:46:25 +0700

    them thanh toan gio hang

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,16 @@ var vm = new Vue({
   el: "#app",
   data: {
     title: "Ao Ba Lỗ Nam Cao Cấp ABL01",
-    src: 'https://salt.tikicdn.com/cache/w444/ts/product/ab/72/5d/d1a0412e1f30f1bc9abca6b90c558bce.jpg',
-    url: "https://tiki.vn/product-p76416270.html?spid=76416300&src=deal-hot",
+    imageProduct: '',
     target: '_blank',
     brand: 'Xám XL',
     price: 21000,
-    color: 'red',
+    color: '',
     isToast: false, 
     displaytest: "",
     bgColor: '#6f99aa',
     txtColor: '#fff',
+    isDisplay: false,
     users: [
       { username: 'luccui', age: 20, address: 'Tra Vinh'},
       { username: 'admin', age: 23, address: 'TpHCM'},
@@ -20,7 +20,30 @@ var vm = new Vue({
       { username: 'testuser2', age: 44, address: 'Ha Noi'},
       { username: 'testuser3', age: 51, address: 'Ha Noi'},
       { username: 'testuser4', age: 13, address: 'Ha Noi'},
-    ]
+    ],
+    detailProduct: [
+      {
+        imageUrl: 'https://salt.tikicdn.com/cache/w444/ts/product/6c/32/45/ea3a36fce939c5536d2b6020ae0c1267.jpg',
+        quality: 10,
+        price: 25000,
+        color: 'blue'
+      },
+      {
+        imageUrl: 'https://salt.tikicdn.com/cache/w444/ts/product/6b/58/9c/62b09db1abc877a80c96a44670249f89.jpg',
+        quality: 5,
+        price: 30000,
+        color: 'orange'
+      },
+      {
+        imageUrl: 'https://salt.tikicdn.com/cache/w444/ts/product/2b/d5/1c/4669488895fafda5b7edf2f5dfbcd77f.jpg',
+        quality: 2,
+        price: 20000,
+        color: 'white'
+      }
+    ],
+    options: '',
+    count: 0,
+    inputCounter: ''
   }, 
   methods: {
     formatPrice() {
@@ -41,8 +64,45 @@ var vm = new Vue({
       this.isToast = !this.isToast; 
       this.bgColor = '#fff';
       this.txtColor = '#6f99aa';
-      
-      this.displaytest = `Sản phẩm: ${this.title}<br/>Giá: ${this.price}, Màu sắc: ${this.color}`
+      this.modelBody = `
+      <div class="table">
+        <table>
+            <thead>
+                <tr>
+                    <th>Tên sản phẩm</th>
+                    <th>Số lượng</th> 
+                    <th>Màu sắc</th> 
+                    <th>Giá</th> 
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td> ${this.title}</td>
+                    <td><input type="number" id="mount" @click="set(this)"></td>
+                    <td> 
+                      <select id="selectCl" >
+                        ${this.setOption }
+                      </select>
+                    </td>
+                    <td>${this.formatPrice(this.price)}</td>
+                </tr>
+                
+            </tbody>
+        </table>
+    </div>
+    <div class="total"><p>Total: ${this.formatPrice(this.price * this.count)}</p></div>
+      `
+      this.isDisplay = !this.isDisplay;
+    },
+    selectProduct(color, price) {
+      this.color = color;
+      this.price = price; 
+    },
+    setImage(image) {
+      this.imageProduct = image;
+    },
+    closeModal() {
+      this.isDisplay = !this.isDisplay;
     }
   }, computed: {
     objectBgClasses: function() {
@@ -53,9 +113,39 @@ var vm = new Vue({
         borderRadius: '5px'
       };
     },
+    getColors() {
+      return this.detailProduct.map(e => {
+        return { 
+          color: e.color,
+          image: e.imageUrl
+        };
+      });
+    },
     userFilter() {
       return this.users.filter(e => { return e.age > 20 })
-    }
+    },
+    setMainImageProd() {
+      if(this.imageProduct.length == 0) {
+        this.color = this.detailProduct[0].color; 
+        this.setImage(this.detailProduct[0].imageUrl);
+      }  
+      if(this.color == this.detailProduct[0].color) {
+        this.setImage(this.detailProduct[0].imageUrl);
+      }
+      if(this.color == this.detailProduct[1].color) {
+        this.setImage(this.detailProduct[1].imageUrl);
+      }
+      if(this.color == this.detailProduct[2].color) {
+        this.setImage(this.detailProduct[2].imageUrl);
+      }
+      return this.imageProduct;
+    },
+    setOption() {
+      this.detailProduct.map(e => {
+        this.options += `<option>${e.color}</option>`;
+      })
+      return this.options;
+    }, 
   }
 });
 
